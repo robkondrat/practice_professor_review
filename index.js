380c559e82b787aae2a74da201b15c29dd865a47
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-05-14 11:57:09 -0500

    initial setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.PORT || 3000
+
+
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API'})
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
